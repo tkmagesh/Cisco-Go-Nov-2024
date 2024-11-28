ec2f30af435ceb77829346271e30bccf5badbc85
Magesh Kuppan <tkmagesh77@gmail.com>
2024-11-28 09:27:02 +0530

    pointers

---
+++ 07-pointers/01-example.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	x = 100
+
+	var xPtr *int
+	xPtr = &x //pointer to x
+	fmt.Println("xPtr : ", xPtr)
+
+	// dereferencing - accessing underlying the data using the address
+	fmt.Println("*xPtr : ", *xPtr)
+
+	*xPtr += 200
+	fmt.Println("x : ", x)
+
+	fmt.Println("Before incrementing, x :", x)
+	increment(&x)
+	fmt.Println("After incrementing, x :", x)
+
+	a, b := 100, 200
+	fmt.Printf("Before swapping, a = %d & b = %d\n", a, b)
+	swap( /* ? */ )
+	fmt.Printf("After swapping, a = %d & b = %d\n", a, b)
+}
+
+func increment(no *int) {
+	fmt.Println("&no :", no)
+	*no++
+}
+
+func swap( /* ? */ ) { //NO return result
+	/* ? */
+}
